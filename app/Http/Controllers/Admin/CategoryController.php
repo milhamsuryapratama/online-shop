ec3b1d98fb4a9c9f9fefe22b8611bc6710f3aaf8
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-13 19:40:44 +0700

    create category controller resource

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategory;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['category'] = Category::all();
+        return view('admin/category/index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategory $storeCategory)
+    {
+        $storeCategory->validated();
+
+        Category::create([
+            'category_name' => $storeCategory->category_name
+        ]);
+
+        return redirect()->back()->withInput()->with('success', 'Category has been created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreCategory $storeCategory, $id)
+    {
+        $storeCategory->validated();
+
+        Category::find($id)->update([
+            'category_name' => $storeCategory->category_name
+        ]);
+
+        return redirect()->back()->withInput()->with('success', 'Category has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        if($category->delete()) {
+            return redirect()->route('category.index')->with('success', 'Category has been deleted');
+        }
+    }
+}
