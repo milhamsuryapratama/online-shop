9d0033eabcb9b8d6620202a208e326f81b26ec4c
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-13 21:50:26 +0700

    install laravel ui and redesign register and login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,35 @@
+@extends('web/master')
+
+@section('content')
+    <div class="product-big-title-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-bit-title text-center">
+                        <h2>Login</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="single-product-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container" style="width: 30%;">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
