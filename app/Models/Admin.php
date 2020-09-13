8fd31f76de208a97b6f665fd6b0990ec3a741eb8
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-13 19:38:41 +0700

    create admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+}
