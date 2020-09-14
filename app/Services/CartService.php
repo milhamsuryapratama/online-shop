24587ecd7648181909fdab0bc81d8323d1e5be99
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 18:14:28 +0700

    fixing validation qty and stock in cart service

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -6,8 +6,10 @@ namespace App\Services;
 
 use App\Models\Cart;
 use App\Models\Product;
+use App\Repositories\CartRespository;
 use Exception;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartService
 {
@@ -34,6 +36,7 @@ class CartService
     public static function changeQty($data)
     {
         $cart = Cart::whereProductId($data['product_id'])->whereUserId(Auth::id())->first();
+
         if (!is_null($data['action']) && $data['action'] == 'plus') {
             if (($cart->qty + 1) > $cart->product->stock) {
                 return response()->json([
@@ -41,8 +44,9 @@ class CartService
                     'message' => 'Failed to add cart !'
                 ], 400);
             }
-            $cart->qty += 1;
-            $cart->update();
+
+            $newCart = CartRespository::updateQty($data['product_id'],Auth::id(),'plus');
+
         } else if (!is_null($data['action']) && $data['action'] == 'minus') {
             if (($cart->qty - 1) < 1) {
                 return response()->json([
@@ -50,8 +54,9 @@ class CartService
                     'message' => 'Failed to add cart !'
                 ], 400);
             }
-            $cart->qty -= 1;
-            $cart->update();
+
+            $newCart = CartRespository::updateQty($data['product_id'],Auth::id(),'minus');
+
         } else {
             if ($data['val'] > $cart->product->stock) {
                 return response()->json([
@@ -59,16 +64,19 @@ class CartService
                     'message' => 'Failed to add cart !'
                 ], 400);
             }
-            $cart->qty = $data['val'];
-            $cart->update();
+
+            $newCart = CartRespository::updateQty($data['product_id'],Auth::id(),'', $data['val']);
         }
+
+        $total = CartRespository::getTotal();
 
         return response()->json([
             'success' => 1,
             'message' => 'Success',
             'data' => [
-                'qty' => $cart->qty,
-                'subtotal' => $cart->qty * $cart->product->price
+                'qty' => $newCart->qty,
+                'subtotal' => $newCart->qty * $newCart->product->price,
+                'total' => $total
             ]
         ], 200);
     }
