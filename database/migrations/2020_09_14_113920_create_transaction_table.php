e29aee09c601929fe2aa6482201fd5288c6fd74c
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 20:24:23 +0700

    create transaction and transaction detail migrations

---
+++ database/migrations/2020_09_14_113920_create_transaction_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('total');
+            $table->string('province');
+            $table->string('city');
+            $table->char('postcode', 5);
+            $table->text('address');
+            $table->enum('status', ['process', 'delivered'])->default('process');
+            $table->enum('payment_precess', ['Y', 'N'])->default('N');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
