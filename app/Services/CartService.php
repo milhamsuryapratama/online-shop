866084689267deafe82e9a4233f369c348ce61af
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 14:16:15 +0700

    add change qty static function to validate stock

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -30,4 +30,46 @@ class CartService
         $cart->qty += $data['qty'];
         return $cart->update();
     }
+
+    public static function changeQty($data)
+    {
+        $cart = Cart::whereProductId($data['product_id'])->whereUserId(Auth::id())->first();
+        if (!is_null($data['action']) && $data['action'] == 'plus') {
+            if (($cart->qty + 1) > $cart->product->stock) {
+                return response()->json([
+                    'success' => 0,
+                    'message' => 'Failed to add cart !'
+                ], 400);
+            }
+            $cart->qty += 1;
+            $cart->update();
+        } else if (!is_null($data['action']) && $data['action'] == 'minus') {
+            if (($cart->qty - 1) < 1) {
+                return response()->json([
+                    'success' => 0,
+                    'message' => 'Failed to add cart !'
+                ], 400);
+            }
+            $cart->qty -= 1;
+            $cart->update();
+        } else {
+            if ($data['val'] > $cart->product->stock) {
+                return response()->json([
+                    'success' => 0,
+                    'message' => 'Failed to add cart !'
+                ], 400);
+            }
+            $cart->qty = $data['val'];
+            $cart->update();
+        }
+
+        return response()->json([
+            'success' => 1,
+            'message' => 'Success',
+            'data' => [
+                'qty' => $cart->qty,
+                'subtotal' => $cart->qty * $cart->product->price
+            ]
+        ], 200);
+    }
 }
