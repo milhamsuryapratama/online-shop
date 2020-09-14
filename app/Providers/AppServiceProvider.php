eebe8a5b06143d874719bd642d5a58fae508f1cf
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 18:11:10 +0700

    total and count cart for cart header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
     }
 
     /**
@@ -23,6 +27,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('currency', function($expression) {
+            return "Rp. <?php echo number_format($expression, 0, ', ', '.'); ?>";
+        });
+
+        view()->composer('*', function ($view)
+        {
+            if (Auth::check()) {
+
+                $cart = Cart::join('products', 'products.id', '=', 'carts.product_id')
+                    ->where('carts.user_id', Auth::id())
+                    ->get([
+                        DB::raw('SUM(carts.qty * products.price) as total'),
+                        DB::raw('COUNT(carts.id) as count')
+                    ]);
+
+                //...with this variable
+                $view->with('cartHead', $cart);
+            }
+        });
     }
 }
