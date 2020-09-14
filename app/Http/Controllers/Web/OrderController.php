a49e0a4eabac8fe73b1863b993c6d3d2f44bd591
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 22:04:04 +0700

    create order controller index function

---
+++ app/Http/Controllers/Web/OrderController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PaidRequest;
+use App\Models\Transaction;
+use App\Repositories\OrderRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $data['order'] = Transaction::orderBy('created_at', 'DESC')->whereUserId(Auth::id())->paginate(3);
+        return view('web/order', $data);
+    }
+}
