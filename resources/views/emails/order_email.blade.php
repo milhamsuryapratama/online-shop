71255ebbd8a57bfcdee9dd332b846c895aa98467
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 20:27:53 +0700

    create order email function and template

---
+++ resources/views/emails/order_email.blade.php
@@ -0,0 +1,55 @@
+@component('mail::message')
+    Hi <strong>{{ Auth::user()->name }}</strong>, You have just made a transaction with the transaction code <strong>PROVO-{{ $transaction->id }}</strong>, along with a list of your orders
+
+<style>
+    #customers {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+
+    #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+    #customers tr:hover {background-color: #ddd;}
+
+    #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
+</style>
+
+<table id="customers">
+    <tr>
+        <th>NO</th>
+        <th>Produk</th>
+        <th>Qty</th>
+        <th>Price</th>
+        <th>Subtotal</th>
+    </tr>
+    @foreach($cart as $c)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $c->product->product_name }}</td>
+            <td>{{ $c->qty }}</td>
+            <td>{{ $c->product->price }}</td>
+            <td>{{ $c->qty * $c->product->price }}</td>
+        </tr>
+    @endforeach
+    <tr>
+        <td>Total</td>
+        <td colspan="4">{{ $transaction->total }}</td>
+    </tr>
+</table>
+
+<br>
+Thanks,<br>
+MyOnlineShop
+@endcomponent
