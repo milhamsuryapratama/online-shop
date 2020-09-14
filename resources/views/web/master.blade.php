3a6157665ae6df5e2c236b91099ebff9ec90f137
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 18:11:50 +0700

    show total and count data cart in header

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -84,7 +84,7 @@
 
             <div class="col-sm-6">
                 <div class="shopping-item">
-                    <a href="cart.html">Cart - <span class="cart-amunt">$100</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
+                    <a href="{{ URL::to('cart') }}">Cart - <span class="cart-amunt">{{ $cartHead[0]->total }}</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">{{ $cartHead[0]->count }}</span></a>
                 </div>
             </div>
         </div>
