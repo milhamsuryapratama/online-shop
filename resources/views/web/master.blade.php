e184477af08baabfabd4dc78cd1b2453ada96d12
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-14 21:58:05 +0700

    fixing menu header

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -104,14 +104,9 @@
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="index.html">Home</a></li>
-                    <li><a href="shop.html">Shop page</a></li>
-                    <li><a href="single-product.html">Single product</a></li>
-                    <li><a href="cart.html">Cart</a></li>
-                    <li><a href="checkout.html">Checkout</a></li>
-                    <li><a href="#">Category</a></li>
-                    <li><a href="#">Others</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li class="active"><a href="{{ URL::to('/') }}">Home</a></li>
+                    <li><a href="{{ URL::to('cart') }}">Cart</a></li>
+                    <li><a href="{{ URL::to('orders') }}">Orders</a></li>
                 </ul>
             </div>
         </div>
