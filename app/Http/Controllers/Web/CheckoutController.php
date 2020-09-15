1f70df9746a08e361e04fb030db8973f96bf3ec6
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-15 07:47:06 +0700

    fixing send mail and session

--- app/Http/Controllers/Web/CheckoutController.php
+++ app/Http/Controllers/Web/CheckoutController.php
@@ -30,7 +30,8 @@ class CheckoutController extends Controller
         $cart = Cart::whereUserId(Auth::id())->get();
 
         $transaction = CheckoutRepository::store($checkoutRequest, $total, $cart);
-        Mail::to('nsyclda@gmail.com')->send(new OrderEmail($transaction, $cart));
-        return redirect()->to('orders')->with('success', 'Congratulations, your order with the code PROVO-'.$transaction->id.' has been successfully placed');
+        Mail::to(Auth::user()->email)->send(new OrderEmail($transaction, $cart));
+        return redirect()->to('orders')->with('success', 'Congratulations, your order with the code PROVO-' . $transaction->id . ' has been successfully placed');
+
     }
 }
