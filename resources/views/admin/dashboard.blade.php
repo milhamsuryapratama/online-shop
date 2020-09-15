68b909935320fa0c840c416b9f3c7035712684e2
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-15 16:16:02 +0700

    create dashboard controller and view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.master')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/">Beranda</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <center><h2>Welcome to My Online Shop</h2></center>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
