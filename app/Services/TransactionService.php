f06e2867bdcae1042896ebd5625e464899195246
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-15 10:08:34 +0700

    create transaction service to validate payment status

---
+++ app/Services/TransactionService.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Transaction;
+
+class TransactionService
+{
+    public static function validatePayment($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        if ($transaction->payment_process != 'Y') {
+            throw new \Exception('Cannot delivered, this order has not been paid yet');
+        }
+
+        $transaction->status = 'delivered';
+        if ($transaction->save()) {
+            return $transaction;
+        }
+    }
+}
