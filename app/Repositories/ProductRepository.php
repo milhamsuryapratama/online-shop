57ab61e16466dea9ff5ef41ef1b8584455fa1bc4
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-15 09:54:40 +0700

    create product repository to store and update product

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+
+class ProductRepository
+{
+    public static function store($data, $name)
+    {
+        return Product::create([
+            'product_name' => $data['product_name'],
+            'price' => $data['price'],
+            'stock' => $data['stock'],
+            'category_id' => $data['category'],
+            'description' => $data['description'],
+            'slug' => Str::slug($data['product_name']),
+            'picture' => $name
+        ]);
+    }
+
+    public static function update($data, $id, $name)
+    {
+        return Product::find($id)->update([
+            'product_name' => $data['product_name'],
+            'price' => $data['price'],
+            'stock' => $data['stock'],
+            'category_id' => $data['category'],
+            'description' => $data['description'],
+            'slug' => Str::slug($data['product_name']),
+            'picture' => $name
+        ]);
+    }
+}
