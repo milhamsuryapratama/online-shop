9089838a96ce82153b48a263a6b275fec3531e42
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-16 08:40:24 +0700

    add product relation in category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['category_name'];
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+
+    }
 }
